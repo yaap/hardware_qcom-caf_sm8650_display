LineageOS/android_hardware_qcom_display

sdm: Conditionally revert "sdm: Implement ‘Null Display’ in SDM core"
This commit conditionally reverts the add of the member function
`IsDisplayHWAvailable()` from commit
24255680b2c02c809040594c8e7536837118754d.

Change-Id: I9ea381906e98134069993e91429594c59d17efc3

sdm/include/private/resource_interface.h
@@ -143,7 +143,9 @@ class ResourceInterface {
   virtual uint32_t GetMixerCount() = 0;
   virtual void HandleTUITransition(Handle display_ctx, bool tui_active) = 0;
   virtual DisplayError SetBlendSpace(Handle display_ctx, const PrimariesTransfer &blend_space) = 0;
+#ifndef SDMCORE_HAS_IS_DISPLAY_HW_AVAILABLE_FUNC
   virtual bool IsDisplayHWAvailable() = 0;
+#endif
 };
 
 }  // namespace sdm

yaap/hardware_qcom-caf_sm8650_display

sdm/include/private/resource_interface.h
@@ -118,7 +118,9 @@ class ResourceInterface {
   virtual DisplayError SetBlendSpace(Handle display_ctx, const PrimariesTransfer &blend_space) = 0;
   virtual void HandleTUITransition(Handle display_ctx, bool tui_active) = 0;
   virtual void GetDSConfig(Handle display_ctx, DestScaleInfoMap *dest_scale_info_map) = 0;
+#ifdef SDMCORE_HAS_IS_DISPLAY_HW_AVAILABLE_FUNC
   virtual bool IsDisplayHWAvailable() = 0;
+#endif
 };
 
 }  // namespace sdm