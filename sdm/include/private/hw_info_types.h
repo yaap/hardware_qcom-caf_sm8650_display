967a9fbd539fd8349393d31de12336349af035cb
John Galt <johngaltfirstrun@gmail.com>
2026-01-13 13:10:32 -0500

    Revert "sdm: color_manager: leave ddr_version out of PPHWAttributes"
    This reverts commit 3ac0279b28361d5bee8ef37949f7351cdaff2e84.

--- sdm/include/private/hw_info_types.h
+++ sdm/include/private/hw_info_types.h
@@ -369,7 +369,7 @@ const int  kPipeDmaLimit       = (1 << 1);
 const int  kPipeScalingLimit   = (1 << 2);
 const int  kPipeRotationLimit  = (1 << 3);
 
-struct LegacyHWResourceInfo {
+struct HWResourceInfo {
   uint32_t hw_version = 0;
   uint32_t num_dma_pipe = 0;
   uint32_t num_vig_pipe = 0;
@@ -442,9 +442,6 @@ struct LegacyHWResourceInfo {
   bool skip_inline_rot_threshold = false;
   bool has_noise_layer = false;
   uint32_t dsc_block_count = 0;
-};
-
-struct HWResourceInfo : LegacyHWResourceInfo {
   DDRVersion ddr_version = kDDRVersion5;
 };
 
