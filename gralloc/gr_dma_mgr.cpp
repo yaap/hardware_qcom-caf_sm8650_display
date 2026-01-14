2dbad1384870ba67a7c0042e9717ad61ce351b52
John Galt <johngaltfirstrun@gmail.com>
2026-01-14 16:00:43 -0500

    gralloc: make DmaManager::GetInstance one time init

--- gralloc/gr_dma_mgr.cpp
+++ gralloc/gr_dma_mgr.cpp
@@ -43,6 +43,7 @@
 #include <dlfcn.h>
 #include <string>
 #include <utility>
+#include <mutex>
 #include <vector>
 
 #include "gr_utils.h"
@@ -74,12 +75,15 @@ DmaManager::DmaManager() {
 }
 
 DmaManager *DmaManager::GetInstance() {
-  if (!dma_manager_) {
+  static std::once_flag once;
+  std::call_once(once, []() {
     dma_manager_ = new DmaManager();
     dma_manager_->enable_logs_ = property_get_bool(ENABLE_LOGS_PROP, 0);
     dma_manager_->GetUncachedHeapUsage();
     dma_manager_->GetCameraPreviewPerms();
-  }
+    dma_manager_->GetSecurePreviewOnly();
+  });
+
   return dma_manager_;
 }
 
